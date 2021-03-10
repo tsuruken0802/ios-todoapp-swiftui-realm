1949c266a090b4996f4bd4c5e1e247624cf72782
鶴本賢太朗 <tsurumotokentarou@gmail.com>
2021-03-10 11:52:45 +0900

    first commit

--- Todo/TodoApp.swift
+++ Todo/TodoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TodoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TodoListView()
         }
     }
 }
---
+++ Todo/Views/TodoListView.swift
@@ -0,0 +1,27 @@
+//
+//  TodoListView.swift
+//  Todo
+//
+//  Created by 鶴本賢太朗 on 2021/03/10.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                List {
+                    
+                }
+            }
+            .navigationTitle("Todos")
+        }
+    }
+}
+
+struct TodoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoListView()
+    }
+}
